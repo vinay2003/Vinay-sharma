0d2c12f1fc6a836825d7f5c089fe798959442175
vinay2003 <66741516+vinay2003@users.noreply.github.com>
2020-12-08 22:29:25 +0530

    Create Logical_AND.c

---
+++ Logical_AND.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a, b;
+printf("Enter the first value:\n");
+scanf("%d",&a);
+printf("Enter the second value:\n");
+scanf("%d",&b);
+printf("\n %d", (a>b)&&(a!=b));
+}
