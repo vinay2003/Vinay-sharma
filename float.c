4b1da3a7fc2a93fc7d79b665507597e270d4e9d3
vinay2003 <66741516+vinay2003@users.noreply.github.com>
2020-12-18 21:18:26 +0530

    float.c

---
+++ float.c
@@ -0,0 +1,16 @@
+//printf("\t\t%d",printf("\nfloat"));
+//printf("\t%d",printf("\n%f",num));
+//printf("\t\t%d",printf("\n%s",status));
+#include<stdio.h>
+void main()
+{
+    float num =8.88;
+    char status[10]= "'out'";
+   printf("\t\t%d",printf("\nfloat"));
+   printf("\t%d",printf("\n%f",num));
+   printf("\t\t%d",printf("\n%s",status));
+}
+//ANS :
+//float 6
+//8.88000 9
+//'out' 6
